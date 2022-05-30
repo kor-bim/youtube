3b6663d0b81c128055fc1a6743400e37879d8406
dbsgksqlschl@gmail.com <hyuof024852>
2022-05-30 16:58:12 +0900

    project init

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -7,16 +7,16 @@ class Youtube {
         this.key = key;
     }
 
-    mostPopular() {
-        return fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`, this.requestOptions)
-            .then(response => response.json())
-            .then(result => result.items);
+    async mostPopular() {
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`, this.requestOptions);
+        const result = await response.json();
+        return result.items;
     }
 
-    search(query) {
-        return fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`, this.requestOptions)
-            .then(response => response.json())
-            .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+    async search(query) {
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`, this.requestOptions);
+        const result = await response.json();
+        return result.items.map(item => ({...item, id: item.id.videoId}));
     }
 }
 
