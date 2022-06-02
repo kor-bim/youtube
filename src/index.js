2ad6baa2c727e8985cd8ba4e144a32cb8a5b87cc
kor-bim <dbsgksqlschl@gmail.com>
2022-06-02 16:28:39 +0900

    project init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import Youtube from './service/youtube';
 
 const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
-ReactDOM.render(
-    <React.StrictMode>
-        <App youtube={youtube}/>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App youtube={youtube}/>);
