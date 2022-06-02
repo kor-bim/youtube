e2e32e4b9c7db2f5ff57902b59e051c971fa18ec
kor-bim <dbsgksqlschl@gmail.com>
2022-06-02 16:46:09 +0900

    project init

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
@@ -12,12 +12,14 @@ function App({youtube}) {
         setSelectedVideo(video);
     };
 
-    const search = query => {
+    const search = useCallback(query => {
         setSelectedVideo(null);
         youtube
             .search(query) //
-            .then(videos => {setVideos(videos)});
-    };
+            .then(videos => {
+                setVideos(videos)
+            });
+    },[youtube]);
 
     useEffect(() => {
         // noinspection JSCheckFunctionSignatures
