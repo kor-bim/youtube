ed988b9e2108cbcf1510e245f8165e423e608660
kor-bim <dbsgksqlschl@gmail.com>
2022-06-02 16:41:56 +0900

    project init

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,7 +1,7 @@
 import styles from './search_header.module.css';
-import React, {useRef} from 'react';
+import React, {memo, useRef} from 'react';
 
-const SearchHeader = ({onSearch}) => {
+const SearchHeader = memo(({onSearch}) => {
     const inputRef = useRef();
     const handleSearch = () => {
         const value = inputRef.current.value;
@@ -16,7 +16,6 @@ const SearchHeader = ({onSearch}) => {
             handleSearch();
         }
     };
-    console.log('sdf')
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
@@ -39,6 +38,6 @@ const SearchHeader = ({onSearch}) => {
             </button>
         </header>
     );
-};
+});
 
 export default SearchHeader;
